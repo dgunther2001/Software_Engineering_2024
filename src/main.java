9ee6b519c713ee4511a2abd8e86305082f2557d9
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2024-08-30 14:41:16 -0400

    Added java doc comments to methods

--- src/main.java
+++ src/main.java
@@ -1,10 +1,21 @@
 import java.util.*;
 
 class factorial {
+
+    /**
+     * @param  args 
+     * @return 
+     * 
+     */
     public static void main(String[] args) {
         System.out.println(factorial(5));
     }
 
+    /**
+     * @param facValue
+     * @return factorial
+     * 
+     */
     public static int factorial(int facValue) {
         if (facValue == 1 || facValue == 0) {
             return 1;
