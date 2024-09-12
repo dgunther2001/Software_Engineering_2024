ee1c2e83bc00699b879ebf3e4a5229466aae78ec
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2024-09-12 10:40:05 -0400

    Added comments to functions

--- src/Engine/ProtoComputeExec.java
+++ src/Engine/ProtoComputeExec.java
@@ -4,11 +4,20 @@ package engine;
  * Actual execution component of the compute engine.
  */
 public interface ProtoComputeExec {
+    /**
+     * Sends an acknowledgment back to the initializer
+     */
     public void sendAckTokenToInitializaer();         // send an Acknowledgement token
 
-    public DataStream sendDataResponse();
 
+    /**
+     * Sends computed result back to initalizer
+     * @param data
+     */
     public void sendDataResponse(Datastream data);
 
+    /**
+     * Executes the computation
+     */
     public void doComputation();
 }
--- src/Engine/ProtoComputeInit.java
+++ src/Engine/ProtoComputeInit.java
@@ -4,7 +4,15 @@ package engine;
  * An interface between the user and the actual execution component of the compute engine
  */
 public interface ProtoComputeInit {
+    /**
+     * 
+     * @param DataStream
+     */
     public void sendComputeRequest(DataStream Data);
 
+    /**
+     * @param None
+     * @return
+     */
     public AckToken processExecAck();
 }
