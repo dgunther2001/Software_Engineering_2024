c2b50c32906b0332f26f7b17c3446af696ab2a3a
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2024-09-19 10:19:50 -0400

    Update IntegrationTest.java

--- IntegrationTest.java
+++ IntegrationTest.java
@@ -1,7 +1,13 @@
 package test;
 
+/**
+ * System level integration tests
+ */ 
 public class IntegrationTest {
 
+  /**
+   Calls a user request that percolates through the entire system
+   */
   @Test
   public void integrationTest() {
     // simply send a user request and validate the output
