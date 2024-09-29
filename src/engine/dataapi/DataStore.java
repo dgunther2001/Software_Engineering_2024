28dd4153c2eedc153e75b77c5d68286df33a2a82
Nico DeMilio <107527142+NicoPotamus@users.noreply.github.com>
2024-09-29 09:31:21 -0400

    layout dataStore api implementation logic (#34)

--- src/engine/dataapi/DataStore.java
+++ src/engine/dataapi/DataStore.java
@@ -1,17 +1,75 @@
 package engine.dataapi;
 
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Receiving end of the data storage API.
  */
-public class DataStore implements ProtoDataStore{
+public class DataStore implements ProtoDataStore {
+	private DataStream data;
 
+	
     /**
-     * Receives a data request and processes it.
+     * Receives a data request for output and processes it.
+     * we're gonna pass an id as well that we'll make in the controller to have a reference to our obj in the db
+     * @param newData is the data to be appended and/or stored
+     * @param id is the id of the object/list
+     * @param file is the user-specified file
      */
     @Override
-    public DataStream receiveDataRequest(DataStream data) {
-        return null;
+    public DataStream receiveDataRequest(DataStream newData, String id, File file) {
+        // search db or file for id
+    	// if no make new db entry for current list and add value
+    	// if yes append data to existing entry
+    	// return db entry
+    	
+    	
+    	DataStream fileObject = null;
+    	try {
+    		//for reading a file's object
+    		FileInputStream fileIn = new FileInputStream(file);
+        	ObjectInputStream objIn = new ObjectInputStream(fileIn);
+        	
+        	//for writing an object to a file 
+        	FileOutputStream fileOut = new FileOutputStream(file);
+        	ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+        	
+    		 while(true) {//check file for existing entry
+    			 try {
+    				 fileObject = (DataStream) objIn.readObject();
+    				 if(fileObject.getId() == id) {
+    					  break;
+    				 }
+    			 }
+    			 catch (IOException | ClassNotFoundException e) {
+    				 break;
+    			 }
+    		 }
+    		 if(fileObject == null) {//if list wasn't started make a new one 
+    			 fileObject = newData;
+    			 objOut.writeObject(fileObject);
+    			 return fileObject;
+    		 }
+    		 else {//if a list was started add to it
+    			 fileObject.append(newData);
+    			 objOut.writeObject(fileObject);
+    			 return fileObject;
+    		 }
+    	}
+    	catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	return fileObject;//return uninitialized object as a last resort
     }
+    
+    
 
     
 }
--- src/engine/dataapi/DataStream.java
+++ src/engine/dataapi/DataStream.java
@@ -1,8 +1,59 @@
 package engine.dataapi;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Data stream passed to and from the controller and data storage.
  */
-public class DataStream implements ProtoDataStream{
+public class DataStream implements ProtoDataStream, Serializable{
+
+    private List<String> data = new ArrayList<>();
+    private String id;
+
+    /**
+     * Pass in a list of data to be stored.
+     * @param data
+     */
+    public DataStream(List<String> data) {
+        this.data = data;
+        id = UUID.randomUUID().toString();
+
+    }
+    
+    /**
+     * Append Data To List
+     */
+    public DataStream append(DataStream newNum) {
+    	for(String str : newNum.getData()) {
+    		data.add(str);
+    	}
+    	return this;
+    }
+
+    /**
+     * Data Getter
+     * @return
+     */
+    public List<String> getData() {
+        return this.data;
+    }
+    
+    /**
+     * get id
+     */
+    public String getId(){
+    	return this.id;
+    }
+    
+    /**
+     * DataSetter
+     * @param data
+     */
+    public void setData(List<String> data) {
+        this.data = data;
+    }
 
 }
