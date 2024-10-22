efec2352d84022ad76b9840c4ff9ee3f293e60d2
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2024-10-22 12:42:52 -0400

    Added a driver function that reds from an input file (#55)
    * Driver function added
    
    * Adjusted checkstyle naming conventions
    
    * Adjusted brace notation checkstyles.
    
    * Added try catch in driver function string conversion to int

---
+++ src/engine/driver/Driver.java
@@ -0,0 +1,58 @@
+package engine.driver;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import engine.userapi.ProtoUserDataStream;
+import engine.userapi.User;
+import engine.userapi.UserDataInput;
+import engine.userapi.UserDataStream;
+
+/**
+ * 
+ */
+public class Driver {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char delim = args[2].charAt(0);
+		String filePath = args[1];
+		String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        List<String> inputIntegersAsString = Arrays.asList(content.split(","));
+        
+        List<Integer> inputIntegers = new ArrayList<>();
+        
+        for(String s : inputIntegersAsString) {
+        	try {
+        	inputIntegers.add(Integer.valueOf(s));
+        	} catch (Throwable t) {
+        		t.printStackTrace();
+        	}
+        }
+        
+        UserDataInput data = new UserDataInput(delim, inputIntegers);
+        
+        User theUser = new User();
+        
+		ProtoUserDataStream outputData = theUser.sendUserRequest(data);
+		
+		for (String s : outputData.getOutput()) {
+			System.out.print(s + delim);
+		}
+		
+	}
+}
